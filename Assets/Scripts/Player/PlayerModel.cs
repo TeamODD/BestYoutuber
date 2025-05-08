04432c3463475b6e731b7621ba344cccae6f3023
camilie2 <gmsaturn0917@gmail.com>
2025-05-09 02:19:50 +0900

    ererer
    erere

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,14 +1,18 @@
 ﻿using TMPro;
 using UnityEngine;
 using System;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerModel : MonoBehaviour
 {
     [SerializeField] private PlayerPresenter _presenter;
+    [SerializeField] private PlayerView _view;
 
     [SerializeField] private int _stress;
     [SerializeField] private int _famous;
     [SerializeField] private int _subscriber;
+
     private Color originalColor;
     private Color stressIncreaseColor = new Color(0.18f, 0.55f, 0.25f); // 초록색
     private Color stressDecreaseColor = new Color(0.10f, 0.23f, 0.34f); // 파란색
@@ -17,29 +21,29 @@ public class PlayerModel : MonoBehaviour
     public event Action<int> OnFamousChanged;
     public event Action<int> OnSubscriberChanged;
 
-    public void SetPlayerStress(int value)             // 이미지 stress 불러오고 fiillamount 값 100f로 지정 TMP StressText 가져와서 값지정 색상변경
+    public IEnumerator ChangeColorCoroutine(Image image, Color targetColor)
     {
-        Image image = _view.GetImage((int)PlayerView.Images.StressImage);
-        image.fillAmount = value / 100.0f;
-
-        TextMeshProUGUI tmp = _view.GetTmp((int)PlayerView.Tmps.StressText);
-        tmp.text = value.ToString();
-
-        if (originalColor == default)
-            originalColor = image.color;
-
-        // 색상 변화 적용
-        StopAllCoroutines();
-        if (value < 0)
-            StartCoroutine(ChangeColorCoroutine(image, stressDecreaseColor));
-        else if (value > 0)
-            StartCoroutine(ChangeColorCoroutine(image, stressIncreaseColor));
+        image.color = targetColor;
+        yield return new WaitForSeconds(0.3f);
+        image.color = originalColor;
     }
 
     public void UpdatePlayerStress(int value)
     {
         _stress = Mathf.Clamp(_stress + value, 0, 100);
         OnStressChanged?.Invoke(_stress);
+        var image = _view.GetImage((int)PlayerView.Images.StressImage);
+
+        if (originalColor == default)
+            originalColor = image.color;
+        StopAllCoroutines();
+        if (value < 0)
+            StartCoroutine(ChangeColorCoroutine(image, stressDecreaseColor));
+        else if (value > 0)
+            StartCoroutine(ChangeColorCoroutine(image, stressIncreaseColor));
+        Debug.Log("Stress Update");
+        
+        Debug.Log("Invoke StressChanged");
     }
 
     public void UpdatePlayerFamous(int value)
