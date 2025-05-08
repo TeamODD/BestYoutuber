1ea943647d67347474e263a5d4f7fffcc743ab87
sensior101 <ladon429@gmail.com>
2025-05-09 02:01:51 +0900

    5/9 오류

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using System;
 
 public class PlayerModel : MonoBehaviour
@@ -8,20 +9,31 @@ public class PlayerModel : MonoBehaviour
     [SerializeField] private int _stress;
     [SerializeField] private int _famous;
     [SerializeField] private int _subscriber;
+    private Color originalColor;
+    private Color stressIncreaseColor = new Color(0.18f, 0.55f, 0.25f); // 초록색
+    private Color stressDecreaseColor = new Color(0.10f, 0.23f, 0.34f); // 파란색
 
-    public int PreviousStress = 100;
     public event Action<int> OnStressChanged;
     public event Action<int> OnFamousChanged;
     public event Action<int> OnSubscriberChanged;
 
-    public void IncreaseStress(int value)
+    public void SetPlayerStress(int value)             // 이미지 stress 불러오고 fiillamount 값 100f로 지정 TMP StressText 가져와서 값지정 색상변경
     {
-        UpdatePlayerStress(value);
-    }
+        Image image = _view.GetImage((int)PlayerView.Images.StressImage);
+        image.fillAmount = value / 100.0f;
 
-    public void DecreaseStress(int value)
-    {
-        UpdatePlayerStress(-value);
+        TextMeshProUGUI tmp = _view.GetTmp((int)PlayerView.Tmps.StressText);
+        tmp.text = value.ToString();
+
+        if (originalColor == default)
+            originalColor = image.color;
+
+        // 색상 변화 적용
+        StopAllCoroutines();
+        if (value < 0)
+            StartCoroutine(ChangeColorCoroutine(image, stressDecreaseColor));
+        else if (value > 0)
+            StartCoroutine(ChangeColorCoroutine(image, stressIncreaseColor));
     }
 
     public void UpdatePlayerStress(int value)
