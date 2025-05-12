ce4f5ef67456da7cd55a3a0d2a3d6d17a2a663fc
chgjs00 <chgjs0604@gmail.com>
2025-05-13 02:04:58 +0900

    BGM 재생

---
+++ Assets/Scripts/BGM2.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BGM2 : MonoBehaviour
+{
+    private static BGM2 instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
